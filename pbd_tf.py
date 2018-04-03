ac1d0aa928abab3ba62974f7ad5cecbfdec1cecc
Angus Fung <angus.fung@mail.utoronto.ca>
2018-04-02 22:55:57 -0400

    get independent weights... hacky, but works!

--- pbd_tf.py
+++ pbd_tf.py
@@ -5,12 +5,10 @@ import os
 import tensorflow as tf
 
 def main(_):
-    logs_path = os.path.join(os.getcwd(), 'logs')
-    
+    # we need to provide all ps and worker info to each server so they are aware of each other
     ps_hosts = FLAGS.ps_hosts.split(",")
     worker_hosts = FLAGS.worker_hosts.split(",")
     
-
     # create a cluster from the parameter server and worker hosts.
     cluster = tf.train.ClusterSpec({"ps": ps_hosts, "worker": worker_hosts})
     
@@ -19,11 +17,20 @@ def main(_):
                             job_name=FLAGS.job_name,
                             task_index=FLAGS.task_index)
                             
+    # log each worker seperately for tensorboard
+    logs_path = os.path.join(os.getcwd(), 'logs'.format(FLAGS.task_index))
+                            
     if FLAGS.job_name == "ps":
         server.join()
     elif FLAGS.job_name == "worker":
         
-        # assigns ops to the local worker by default.
+        # explictely place weights and hyperparameters on the worker servers to prevent sharing
+        # otherwise replica_device_setter will put them on the ps
+        with tf.device("/job:worker/task:{}".format(FLAGS.task_index)):
+            theta = tf.get_variable('theta'.format(FLAGS.task_index), initializer=tf.random_uniform(shape=[2]))
+            h = tf.get_variable('h', initializer=tf.random_uniform(shape=[2]), trainable=False)
+        
+        # use replica_device_setter to automatically set device-ops
         with tf.device(tf.train.replica_device_setter(
             worker_device="/job:worker/task:%d" % FLAGS.task_index,
             cluster=cluster)):
@@ -43,7 +50,6 @@ def main(_):
             tf.summary.scalar('loss', loss)
             merged = tf.summary.merge_all()
 
-            
             
             with tf.train.MonitoredTrainingSession(master=server.target,
                                                 is_chief=(FLAGS.task_index == 0)) as mon_sess:
